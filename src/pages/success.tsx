3366efa7013152b1e74f4ca738c03f7458bdbe09
WorldWideDevelop <bridleland@gmail.com>
2022-02-26 00:22:05 +0800

    🧹 clean up success page

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,7 +1,6 @@
 import { useRouter } from 'next/router'
 import React from 'react'
 import useSWR from 'swr'
-import tw from 'tailwind-styled-components'
 
 import { Container, MaxWrapper } from '@components/common'
 import { fetcher } from '@lib/fetcher'
@@ -17,21 +16,24 @@ export default function Success() {
     fetcher
   )
 
-  if (error) return <div>failed to load</div>
+  let content = (
+    <div>
+      <h1>Checkout Payment Result</h1>
+      <h2>Status: {data?.payment_intent?.status ?? 'loading...'}</h2>
+      <h3>CheckoutSession response:</h3>
+      {/* <PrintObject content={data ?? 'loading...'} />
+   <Cart>
+     <ClearCart />
+   </Cart> */}
+    </div>
+  )
+
+  if (error) content = <div>failed to load</div>
   if (!session_id) push('/')
-  console.log({ data })
-  console.log({ error })
+
   return (
     <Container>
-      <MaxWrapper>
-        <h1>Checkout Payment Result</h1>
-        <h2>Status: {data?.payment_intent?.status ?? 'loading...'}</h2>
-        <h3>CheckoutSession response:</h3>
-        {/* <PrintObject content={data ?? 'loading...'} />
-        <Cart>
-          <ClearCart />
-        </Cart> */}
-      </MaxWrapper>
+      <MaxWrapper>{content}</MaxWrapper>
     </Container>
   )
 }
